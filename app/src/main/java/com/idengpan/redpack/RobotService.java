39ee3af81af880c79cfd4a5e0cb64601e48ee0ad
ben.deng <ben.deng@vipshop.com>
2016-01-20 14:47:07 +0800

    开始硬编码

--- app/src/main/java/com/idengpan/redpack/RobotService.java
+++ app/src/main/java/com/idengpan/redpack/RobotService.java
@@ -11,7 +11,20 @@ public class RobotService extends AccessibilityService {
     //整个窗口视图的快照
     private AccessibilityNodeInfo mNodeInfo = null;
 
-    private int robbed = 0;
+
+    private void processWeixin(){
+        List localList = getRootInActiveWindow().findAccessibilityNodeInfosByText("领取红包");
+        if(localList != null && localList.size()>0){
+            AccessibilityNodeInfo parent = ((AccessibilityNodeInfo)localList.get(localList.size()-1)).getParent();
+            if(parent!=null){
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+
+    }
+
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         mNodeInfo = event.getSource();
@@ -19,17 +32,19 @@ public class RobotService extends AccessibilityService {
             return;
         }
 
+
         //窗口内容有变化，判断是否有红包
-        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED){
+        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED){//2048
             List<AccessibilityNodeInfo> hbs = mNodeInfo.findAccessibilityNodeInfosByText("微信红包");
+
             if(hbs.size() > 0){
                 AccessibilityNodeInfo curNodeInfo = hbs.get(hbs.size() - 1);
                 curNodeInfo.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
             }
         }
 
-        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED){
-            List<AccessibilityNodeInfo> popWindowList = mNodeInfo.findAccessibilityNodeInfosByText("拆红包");
+        if(event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED){//32
+            List<AccessibilityNodeInfo> popWindowList = mNodeInfo.findAccessibilityNodeInfosByText("開");
             if(popWindowList.size() > 0){
                 AccessibilityNodeInfo accessibilityNodeInfo = popWindowList.get(popWindowList.size()-1);
                 accessibilityNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
@@ -41,6 +56,17 @@ public class RobotService extends AccessibilityService {
 
     @Override
     public void onInterrupt() {
-
+         System.out.println("服务onInterrupt");
     }
+
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        //在设置中开启了辅助服务时回调
+        System.out.println("服务onServiceConnected");
+    }
+
+
+
 }
