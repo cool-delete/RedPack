b238cf25b64bbc34c19733d0c78f7e8d96b9ec30
ben.deng <ben.deng@vipshop.com>
2016-01-21 15:29:42 +0800

    还有一点点小bug。

--- app/src/main/java/com/idengpan/redpack/RobotService.java
+++ app/src/main/java/com/idengpan/redpack/RobotService.java
@@ -5,7 +5,6 @@ import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.os.PowerManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -20,19 +19,6 @@ public class RobotService extends AccessibilityService {
     public static boolean isWork = false;
     private boolean isNewPacket = false;
     private long time = 0L;
-
-
-    private void a() {
-        try {
-            Thread.sleep(1000L);
-            Intent localIntent = new Intent(this, MainActivity.class);
-            localIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-            startActivity(localIntent);
-            return;
-        } catch (InterruptedException localInterruptedException) {
-            localInterruptedException.printStackTrace();
-        }
-    }
 
     /**
      * 亮屏解锁
@@ -49,6 +35,11 @@ public class RobotService extends AccessibilityService {
             return;
         }*/
 
+        System.out.println("onAccessibilityEvent：" + Integer.toHexString(event.getEventType()) +";" + event.getText().toString());
+
+        if(event.getText().size() == 0){
+            isNewPacket = true;
+        }
         try {
             if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {//0x40
                 System.out.println("通知事件");
@@ -66,17 +57,16 @@ public class RobotService extends AccessibilityService {
                 }
             }
 
-
-            if (System.currentTimeMillis() - this.time > 15000L) {
-                this.isNewPacket = false;
-            }
-
             //窗口内容改变
             if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {//0x800
                 System.out.println("窗口内容改变事件");
                 long interval = System.currentTimeMillis() - this.time;
                 System.out.println("间隔时间："+interval);
-                if(interval <= 1500 || !isNewPacket){
+                if(!isNewPacket){
+                    return;
+                }
+                if(interval <= 1500){
+                    this.time = System.currentTimeMillis();
                     return;
                 }
                 List<AccessibilityNodeInfo> localList = getRootInActiveWindow().findAccessibilityNodeInfosByText("领取红包");
@@ -84,16 +74,15 @@ public class RobotService extends AccessibilityService {
                     AccessibilityNodeInfo recentNode =  localList.get(localList.size() - 1);
                     if (recentNode.getParent() != null) {
                         this.time = System.currentTimeMillis();
+                        this.isNewPacket = true;
+                        waitTime();
                         recentNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    } /*else {
+                    }else{
                         recentNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                    }*/
-                    this.isNewPacket = true;
+                    }
                 }
             }
 
-            if(!isNewPacket)
-                return;
             //当显示一个PopWIndow时触发
             if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {//0x20
                 System.out.println("POP弹出事件");
@@ -101,56 +90,16 @@ public class RobotService extends AccessibilityService {
                 if (rootNodeInfo != null) {
                     List<AccessibilityNodeInfo> pops = rootNodeInfo.findAccessibilityNodeInfosByText("发了一个红包");
                     if(pops != null && pops.size() >0){
-                        pops.get(pops.size()-1).getParent().getChild(3).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                        //this.time = System.currentTimeMillis();
                         this.isNewPacket = false;
+                        this.time = System.currentTimeMillis();
+                        pops.get(pops.size()-1).getParent().getChild(3).performAction(AccessibilityNodeInfo.ACTION_CLICK);
                     }
                 }
             }
-
-
-
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        //窗口内容有变化，判断是否有红包
-        /*if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {//2048
-            List<AccessibilityNodeInfo> hbs = mNodeInfo.findAccessibilityNodeInfosByText("微信红包");
-
-            if (hbs.size() > 0) {
-                AccessibilityNodeInfo curNodeInfo = hbs.get(hbs.size() - 1);
-                curNodeInfo.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
-        }
-
-        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {//32
-            List<AccessibilityNodeInfo> popWindowList = mNodeInfo.findAccessibilityNodeInfosByText("開");
-            if (popWindowList.size() > 0) {
-                AccessibilityNodeInfo accessibilityNodeInfo = popWindowList.get(popWindowList.size() - 1);
-                accessibilityNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
-        }
-*/
     }
-
-
-    public void process(AccessibilityNodeInfo paramAccessibilityNodeInfo) {
-        try {
-            if (!this.isNewPacket) {
-                return;
-            }
-            if (System.currentTimeMillis() - this.time > 15000L) {
-                this.isNewPacket = false;
-            }
-        } catch (Exception localException) {
-            localException.printStackTrace();
-        }
-    }
-
 
     @Override
     public void onInterrupt() {
