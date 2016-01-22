a001302428921a060761ebf6d7416a7120b52be6
dengpan <idengpan@idengpan.com>
2016-01-23 00:06:01 +0800

    一些小修改，可以识别是否已开启辅助服务，在5.0以上不能自动开启服务，在其他版本系统上尚未测试。bug后面修复

--- app/src/main/java/com/idengpan/redpack/MainActivity.java
+++ app/src/main/java/com/idengpan/redpack/MainActivity.java
@@ -1,16 +1,21 @@
 package com.idengpan.redpack;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +46,57 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        String accessibilityEnabledString = Settings.Secure.getString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+        Log.d("idengpan","accessibilityEnabledString = " + accessibilityEnabledString);//com.idengpan.redpack/com.idengpan.redpack.RobotService
+        if(!accessibilityEnabledString.contains(".RobotService")){//com.idengpan.redpack/.RobotService
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("特别提示");
+            builder.setMessage("辅助服务未开启，是否前往设置开启，以便能自动抢红包？");
+            builder.setPositiveButton("前往设置", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                    startActivityForResult(intent, 0);
+                }
+            });
+
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "服务未开启，自动抢红包功能将不可用", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "com.idengpan.redpack/com.idengpan.redpack.RobotService");
+                Log.e("idengpan","自动开启辅助服务");
+            }
+
+            builder.create().show();
+        }else{
+            Toast.makeText(this,"辅助服务已开启，可以自动抢红包啦！",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0){
+            String accessibilityEnabledString = Settings.Secure.getString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            Log.d("idengpan","accessibilityEnabledString = " + accessibilityEnabledString);//com.idengpan.redpack/com.idengpan.redpack.RobotService
+            if(accessibilityEnabledString.contains(".RobotService")){
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Toast.makeText(this,"设置成功，开始自动抢红包啦！",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(MainActivity.this, "服务未开启，自动抢红包功能将不可用", Toast.LENGTH_SHORT).show();
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                    Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "com.idengpan.redpack/com.idengpan.redpack.RobotService");
+                    Log.e("idengpan", "自动开启辅助服务");
+                    Toast.makeText(MainActivity.this, "已自动为你开启辅助服务", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
     }
 
     @Override
@@ -61,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             //return true;
             Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            startActivity(intent);
+            startActivityForResult(intent, 0);
         }
 
         return super.onOptionsItemSelected(item);
