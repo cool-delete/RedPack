bcd99841fb3ca0634aeda9f94e44799bbbd70860
dengpan <idengpan@idengpan.com>
2016-01-25 23:53:23 +0800

    可以自动返回。还有一个bug，就是会不停的点，需要逻辑进行控制

--- app/src/main/java/com/idengpan/redpack/MainActivity.java
+++ app/src/main/java/com/idengpan/redpack/MainActivity.java
@@ -10,15 +10,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +38,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tv = (TextView)findViewById(R.id.tv);
         final Button btn = (Button)findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                btn.animate().alphaBy(1).alpha(0.1f).setDuration(1500).start();
-                //btn.animate().alphaBy(0.1f).alpha(1).setDuration(1500).start();
+               // btn.animate().alphaBy(1).alpha(0.1f).setDuration(1500).start();
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivityForResult(intent, 0);
             }
         });
 
@@ -49,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         String accessibilityEnabledString = Settings.Secure.getString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
         Log.d("idengpan","accessibilityEnabledString = " + accessibilityEnabledString);//com.idengpan.redpack/com.idengpan.redpack.RobotService
         if(!accessibilityEnabledString.contains(".RobotService")){//com.idengpan.redpack/.RobotService
+            tv.setText(Html.fromHtml("<font color = gray>服务未开启</font>"));
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("特别提示");
             builder.setMessage("辅助服务未开启，是否前往设置开启，以便能自动抢红包？");
@@ -63,17 +69,19 @@ public class MainActivity extends AppCompatActivity {
             builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    tv.setText("服务未开启，自动抢红包功能将不可用");
                     Toast.makeText(MainActivity.this, "服务未开启，自动抢红包功能将不可用", Toast.LENGTH_SHORT).show();
                 }
             });
 
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            if(false){
                 Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "com.idengpan.redpack/com.idengpan.redpack.RobotService");
                 Log.e("idengpan","自动开启辅助服务");
             }
 
             builder.create().show();
         }else{
+            tv.setText(Html.fromHtml("<font color = green>服务已开启</font>"));
             Toast.makeText(this,"辅助服务已开启，可以自动抢红包啦！",Toast.LENGTH_SHORT).show();
         }
     }
@@ -86,10 +94,12 @@ public class MainActivity extends AppCompatActivity {
             Log.d("idengpan","accessibilityEnabledString = " + accessibilityEnabledString);//com.idengpan.redpack/com.idengpan.redpack.RobotService
             if(accessibilityEnabledString.contains(".RobotService")){
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                tv.setText(Html.fromHtml("<font color = green>服务已开启</font>"));
                 Toast.makeText(this,"设置成功，开始自动抢红包啦！",Toast.LENGTH_SHORT).show();
             }else{
+                tv.setText(Html.fromHtml("<font color = gray>服务未开启</font>"));
                 Toast.makeText(MainActivity.this, "服务未开启，自动抢红包功能将不可用", Toast.LENGTH_SHORT).show();
-                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                if(false){//Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
                     Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "com.idengpan.redpack/com.idengpan.redpack.RobotService");
                     Log.e("idengpan", "自动开启辅助服务");
                     Toast.makeText(MainActivity.this, "已自动为你开启辅助服务", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/idengpan/redpack/RobotService.java
+++ app/src/main/java/com/idengpan/redpack/RobotService.java
@@ -6,11 +6,16 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.os.PowerManager;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.util.List;
 
+/**
+ * 从微信主界面进入聊天界面以及从聊天界面返回到主界面都是<b></>TYPE_WINDOW_CONTENT_CHANGED</b></>事件。
+ *
+ */
 public class RobotService extends AccessibilityService {
 
     //整个窗口视图的快照
@@ -35,11 +40,12 @@ public class RobotService extends AccessibilityService {
             return;
         }*/
 
-        System.out.println("onAccessibilityEvent：" + Integer.toHexString(event.getEventType()) +";" + event.getText().toString());
+        System.out.println("onAccessibilityEvent：" + Integer.toHexString(event.getEventType()) + ";" + event.getText().toString());
 
         if(event.getText().size() == 0){
-            isNewPacket = true;
+            //isNewPacket = true;
         }
+
         try {
             if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {//0x40
                 System.out.println("通知事件");
@@ -59,33 +65,42 @@ public class RobotService extends AccessibilityService {
 
             //窗口内容改变
             if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {//0x800
-                System.out.println("窗口内容改变事件");
-                long interval = System.currentTimeMillis() - this.time;
-                System.out.println("间隔时间："+interval);
+                System.out.println("Window Content Change事件");
+                //long interval = System.currentTimeMillis() - this.time;
+                //System.out.println("间隔时间："+interval);
+
+                /*if(interval <= 1500){
+                    this.time = System.currentTimeMillis();
+                    return;
+                }*/
+
+                if(isCanBack()){
+                    isNewPacket = false;
+                    performGlobalAction(GLOBAL_ACTION_BACK);
+                }
+
                 if(!isNewPacket){
                     return;
                 }
-                if(interval <= 1500){
-                    this.time = System.currentTimeMillis();
-                    return;
-                }
+
                 List<AccessibilityNodeInfo> localList = getRootInActiveWindow().findAccessibilityNodeInfosByText("领取红包");
                 if (localList != null && localList.size() > 0) {
-                    AccessibilityNodeInfo recentNode =  localList.get(localList.size() - 1);
+                    AccessibilityNodeInfo recentNode = localList.get(localList.size() - 1);
                     if (recentNode.getParent() != null) {
                         this.time = System.currentTimeMillis();
                         this.isNewPacket = true;
-                        waitTime();
+                        waitTime();//这里等待200ms，为了进入界面联网加载新的红包，否则抢的是旧的，容易失误
                         recentNode.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        System.out.println("Window Content Change点击事件");
                     }else{
                         recentNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
                     }
                 }
             }
 
-            //当显示一个PopWIndow时触发
+            //当显示一个PopWIndow时触发   || 或者进入微信聊天列表
             if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {//0x20
-                System.out.println("POP弹出事件");
+                System.out.println("WINDOW_STATE_CHANGED事件");
                 AccessibilityNodeInfo rootNodeInfo = getRootInActiveWindow();
                 if (rootNodeInfo != null) {
                     List<AccessibilityNodeInfo> pops = rootNodeInfo.findAccessibilityNodeInfosByText("发了一个红包");
@@ -93,6 +108,7 @@ public class RobotService extends AccessibilityService {
                         this.isNewPacket = false;
                         this.time = System.currentTimeMillis();
                         pops.get(pops.size()-1).getParent().getChild(3).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        System.out.println("WINDOW_STATE_CHANGED点击");
                     }
                 }
             }
@@ -100,6 +116,65 @@ public class RobotService extends AccessibilityService {
             e.printStackTrace();
         }
     }
+
+
+    private final static int ENVELOPE_RETURN = 0x12;
+    /**
+     * @param info 当前节点
+     * @param matchFlag 需要匹配的文字
+     * @param type  操作的类型
+     */
+    public void recycle(AccessibilityNodeInfo info, String matchFlag, int type) {
+        if (info != null) {
+            if (info.getChildCount() == 0) {
+                CharSequence desrc = info.getContentDescription();
+                switch (type) {
+                    case ENVELOPE_RETURN://返回
+                        if (desrc != null && matchFlag.equals(info.getContentDescription().toString().trim())) {
+                            if (info.isCheckable()) {
+                                info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            } else {
+                                performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+                            }
+                        }
+                        break;
+                }
+            } else {
+                int size = info.getChildCount();
+                for (int i = 0; i < size ; i++){
+                    AccessibilityNodeInfo childInfo = info.getChild(i);
+                    if (childInfo != null) {
+                        Log.e("idengpan", "index: " + i + " info" + childInfo.getClassName() + " : " + childInfo.getContentDescription()+" : "+info.getText());
+                        recycle(childInfo, matchFlag, type);
+                }
+                }
+
+            }
+        }
+
+    }
+
+
+
+    //是否抢完红包了可以返回？
+    private boolean isCanBack(){
+        List<AccessibilityNodeInfo> localList = getRootInActiveWindow().findAccessibilityNodeInfosByText("查看我的红包记录");
+        return localList != null && localList.size() > 0;
+    }
+
+    /*public boolean performGlobalAction(int action) {
+        IAccessibilityServiceConnection connection =
+                AccessibilityInteractionClient.getInstance().getConnection(mConnectionId);
+        if (connection != null) {
+            try {
+                return connection.performGlobalAction(action);
+            } catch (RemoteException re) {
+                Log.w("idengpan", "不能执行Global Action", re);
+            }
+        }
+        return false;
+    }*/
+
 
     @Override
     public void onInterrupt() {
